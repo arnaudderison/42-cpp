0b49e79c7c2bcd4f3750caa70030a983432dc565
arnaudderison <arnaud.derison@hotmail.com>
2025-05-28 14:38:42 +0200

    laste change

--- module06/ex00/src/main.cpp
+++ module06/ex00/src/main.cpp
@@ -1,7 +1,7 @@
 #include "ScalarConverter.hpp"
 
 int main(int argc, char* argv[]) {
-    if(argc != 2 || !argv[1]) {
+    if(argc != 2) {
         std::cerr << "Invalid argument" << std::endl;
         return 1;
     }
